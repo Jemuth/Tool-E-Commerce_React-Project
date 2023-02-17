9477cf3124b8fa7e0643ddddbbabd39804f630b6
Jemuth <juanjanag@gmail.com>
2023-02-16 21:00:47 -0300

    Agregando imagenes y json

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import CartWidget from './CartWidget';
 const Navbar = () => {
     return (
         <nav>
-            <h1 className='h1'>P-TOOLS <small class="text-muted">Para expertos como tú</small></h1>
+            <h1 className='h1'>P-TOOLS <small className="text-muted">Para expertos como tú</small></h1>
             <div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><a href="#">Eléctricas</a></li>
-                    <li class="list-group-item"><a href="#">Manuales</a></li>
-                    <li class="list-group-item"><a href="#">Equipo de seguridad</a></li>
-                    <li class="list-group-item"><a href="#">Contenedores</a></li>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"><a href="#">Eléctricas</a></li>
+                    <li className="list-group-item"><a href="#">Manuales</a></li>
+                    <li className="list-group-item"><a href="#">Equipo de seguridad</a></li>
+                    <li className="list-group-item"><a href="#">Contenedores</a></li>
                 </ul>
             </div>
             <CartWidget/>
