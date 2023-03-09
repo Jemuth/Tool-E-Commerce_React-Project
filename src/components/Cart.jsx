a7e386f87823067b937df142dda61f76bd8a62ea
Jemuth <juanjanag@gmail.com>
2023-03-09 04:06:49 -0300

    Agregando estilos finales

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -25,12 +25,12 @@ const Cart = () => {
     addDoc(ordersCollection, order)
     .then(({ id }) => console.log(id))
     Swal.fire({
-      title: 'Sweet!',
-      text: 'Modal with a custom image.',
-      imageUrl: 'https://unsplash.it/400/200',
+      title: 'Tu orden ha sido ingresada',
+      text: 'Recibirás en tu correo una copia de la orden de compra',
+      imageUrl: 'https://raw.githubusercontent.com/Jemuth/PreEntregaI-JuanJana/main/public/assets/images/alert.jpg',
       imageWidth: 400,
-      imageHeight: 200,
-      imageAlt: 'Custom image',
+      imageHeight: 300,
+      imageAlt: 'Trabajadores',
       confirmButtonColor: '#ff7919',
     })
   }
@@ -54,24 +54,24 @@ const Cart = () => {
     }
     <div className='checkoutForm'>
     <h3><span style={{ fontWeight: 'bold' }}>Tu orden de compra</span></h3>
-    <p><span style={{ fontWeight: 'bold' }}>Tu total a pagar es: ${totalPrice()}</span></p>
+    <p><span style={{ fontWeight: 'bold' }}>Total a pagar: ${totalPrice()}</span></p>
     <p><span style={{ fontWeight: 'bold', textDecoration: 'underline' }}>Ingresa tus datos</span></p>
     <form>
           <div className="inputForm">
-            <label htmlFor="name">Nombre</label>
-            <input id="name" type="text" defaultValue="John Doe" />
+            <label align="right" htmlFor="name">Nombre</label>
+            <input align="left" id="name" type="text" defaultValue="John Doe" />
           </div>
           <div className="inputForm">
-            <label htmlFor="email">Email</label>
-            <input id="email" type="text" defaultValue="johndoe@gmail.com" />
+            <label align="right" htmlFor="email">Correo@</label>
+            <input align="left" id="email" type="text" defaultValue="johndoe@gmail.com" />
           </div>
           <div className="inputForm">
-            <label htmlFor="phone">Teléfono</label>
-            <input id="phone" type="phone" defaultValue="202-555-0168" />
+            <label align="right" htmlFor="phone">Teléfono</label>
+            <input align="left" id="phone" type="phone" defaultValue="202-555-0168" />
           </div>
           <div className="inputForm">
-            <label htmlFor="adress">Dirección</label>
-            <input id="adress" type="adress" defaultValue="2700 Butterfield Coach Rd" />
+            <label align="right" htmlFor="adress">Dirección</label>
+            <input align="left" id="adress" type="adress" defaultValue="2700 Butterfield Coach Rd" />
           </div>
         </form>
       <button onClick={handleClick} className= "card_button btn mr-2">Enviar orden de compra</button>
